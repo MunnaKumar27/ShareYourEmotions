ee75ddba70e1b482ba530743ca0a15db0d998876
MUNNA KUMAR <96313339+MunnaKumar27@users.noreply.github.com>
2024-12-03 14:05:44 +0530

    Update EditPost.js

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('https://shareyouremotion-backend.onrender.com/'+id)
+    fetch('https://shareyouremotion-backend.onrender.com/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
